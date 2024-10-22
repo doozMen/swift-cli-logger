500ff612f17ac1c8abac1fa863cb148883c024ac
stijn <stijn.willems@mediahuis.com>
2024-10-22 16:42:26 +0200

    update to swift 5.10

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,20 +6,20 @@ import PackageDescription
 let lib = "ClILogger"
 
 let package = Package(
-    name: "swift-cli-logger",
-    platforms: [.macOS(.v13), .iOS(.v13)],
-    products: [.library(name: lib, targets: [lib]),],
-    dependencies: [
-      .package(url: "https://github.com/apple/swift-log.git", from: "1.5.4"),
-      .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0")
-    ],
-    targets: [
-      .executableTarget(name: "cli-example", dependencies: [.target(name: lib)]),
-      .target(
-      name: lib, 
+  name: "swift-cli-logger",
+  platforms: [.macOS(.v13), .iOS(.v13)],
+  products: [.library(name: lib, targets: [lib])],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-log.git", from: "1.5.4"),
+    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.5.0"),
+  ],
+  targets: [
+    .executableTarget(name: "cli-example", dependencies: [.target(name: lib)]),
+    .target(
+      name: lib,
       dependencies: [
         .product(name: "Logging", package: "swift-log"),
-        .product(name: "ArgumentParser", package: "swift-argument-parser")
-      ])
-    ]
+        .product(name: "ArgumentParser", package: "swift-argument-parser"),
+      ]),
+  ]
 )
